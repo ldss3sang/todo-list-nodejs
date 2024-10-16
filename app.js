21a16ff791bfea331bc62137ccf1d8c2ae7c9a18
Sangseok Doe <sangseok.doe@rlcarriers.com>
2024-10-16 12:00:08 -0400

    fixed port

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ mongoose
     console.log("DB connection failed.", err);
   });
 
-app.listen(5000, () => {
-  console.log("Server on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server on {PORT}`);
 });
