bf2c6730226f5cf3d8d728592a9436f7c4f6b07b
Sangseok Doe <sangseok.doe@rlcarriers.com>
2024-10-21 02:56:53 -0400

    added createUser and login

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const User = require("../model/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userController = {};
 
@@ -28,20 +29,23 @@ userController.createUser = async (req, res) => {
   }
 };
 
-// userController.login = async (req, res) => {
-//   try {
-//     const {
-//       body: { email, password },
-//     } = req;
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//         throw new Error("No user found");
-//     }
-//     bcrypt.compareSync(password, user.password);
-
-//   } catch (error) {
-//     res.status(400).json({ status: "fail", error: error.message });
-//   }
-// };
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const {
+      body: { email, password },
+    } = req;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: "ok", user, token });
+      }
+    }
+    throw new Error("Wrong Username or passowrd");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
 
 module.exports = userController;
--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const userSchema = Schema(
   {
@@ -8,16 +10,29 @@ const userSchema = Schema(
       required: true,
     },
     email: {
-      type: Boolean,
+      type: String,
       required: true,
     },
     password: {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
